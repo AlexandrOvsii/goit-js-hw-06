c628f2485cbfbe93775784f8f96750d0c1723fe5
Alexandr Ovsii <sedanvw@gmail.com>
2023-07-17 18:28:56 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,9 +1,3 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-};
-
 const buttonEl = document.querySelector('.change-color');
 const colorSpan = document.querySelector('.color');
 
@@ -13,6 +7,13 @@ function colorChange(){
   document.body.style.backgroundColor = randomColor; //присваиваем значение randomColor document.body.style.backgroundColor
   colorSpan.textContent = randomColor; //записываем в текстконтент colorSpan значение, которое пришло из переменной randomColor
 };
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+};
+
 
 
 buttonEl.addEventListener('click', colorChange); //вешаем слушателя событий
